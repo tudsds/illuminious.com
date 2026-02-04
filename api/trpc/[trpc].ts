2e40f37f420ea4fbfccc4fe95a31513315fe3f3a
tudsds <236897115+tudsds@users.noreply.github.com>
2026-02-03 23:27:08 -0500

    Add nodejs runtime config to API handler

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -3,6 +3,7 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "../../server/routers";
 
 export const config = {
+  runtime: 'nodejs',
   api: {
     bodyParser: false,
   },
